7d0a059f793c0581a9436cda6f9b6c9b026a4252
Ezequiel Matías B. R. <ezequielmbeecherr@gmail.com>
2025-06-29 12:45:45 -0300

    Update and rename act-004_14-05-2025_UTN-FRA.c to 2025-05-14-004.c

--- Actividades/2025-05-14-004.c
+++ Actividades/2025-05-14-004.c
@@ -1,22 +1,22 @@
 #include <stdio.h>      // Biblioteca Estandas de Entradas y Salidas
 
-int main()      // Función principal
+int main()      // FunciÃ³n principal
 {
-    int numero = 1;         // Variable del número natural
+    int numero = 1;         // Variable del nÃºmero natural
     int acumulador = 0;     // Variable del acumulador
     int contador = 0;       // Variable del contador
 
     printf("Se mostrara la suma de los 20 primeros numeros naturales.\n");      // Indico al usuario lo que se va a realizar
 
-    do      // Hacer (Ir a While, línea 17)
+    do      // Hacer (Ir a While, lÃ­nea 17)
     {
         contador++;                 // Aumento el contador
-        acumulador += numero;       // Sumo el número al acumulador
-        numero++;                   // Aumento el número natural
+        acumulador += numero;       // Sumo el nÃºmero al acumulador
+        numero++;                   // Aumento el nÃºmero natural
     }
     while(contador < 20);       // (Desde hacer) Mientras el valor del contador sea menor a 20
 
     printf("Su resultador es %d.", acumulador);     // Indico el resultado
 
-    return 0;       // Devuelve un valor 0 para indicar que el algoritmo se ejecutó correctamente
+    return 0;       // Devuelve un valor 0 para indicar que el algoritmo se ejecutÃ³ correctamente
 }
